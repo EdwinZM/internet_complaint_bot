c2f2c6fd46797d8d2c9ec92fd1383b004a6f1033
Edwin Zwanenburg <edwinzwanenburg@Macs-MacBook-Pro.local>
2021-06-30 14:04:36 -0500

    added tweeting feature

--- internetspeedtwitterbot.py
+++ internetspeedtwitterbot.py
@@ -3,7 +3,7 @@ import time
 
 class InternetSpeedTwitterBot:
     def __init__(self):
-        self.path = "/Users/edwinzwanenburg/Code/chromedriver"
+        self.path = "YOUR CHROMEDRIVER PATH"
         self.driver = webdriver.Chrome(executable_path=self.path)
         self.down = None
         self.up = None
@@ -13,12 +13,50 @@ class InternetSpeedTwitterBot:
         go_btn = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[1]/a/span[4]')
         go_btn.click()
 
-        time.sleep(50)
+        time.sleep(45)
 
         down = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[2]/div/div[2]/span')
         up = self.driver.find_element_by_xpath('//*[@id="container"]/div/div[3]/div/div/div/div[2]/div[3]/div[3]/div/div[3]/div/div/div[2]/div[1]/div[3]/div/div[2]/span')
         self.down = float(down.text)
         self.up = float(up.text)
 
-    def tweet_at_provider(self):
-        print("sending tweet")
+    def tweet_at_provider(self, e, p, user, d, u):
+        self.driver.get("https://twitter.com/")
+
+        time.sleep(2)
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div/main/div/div/div/div[1]/div/div[3]/a[2]')
+        login_btn.click()
+
+        time.sleep(1)
+
+        email = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
+        email.send_keys(f"{e}")
+
+        password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
+        password.send_keys(f"{p}")
+
+        login_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div')
+        login_btn.click()
+
+        time.sleep(2)
+
+        try:
+            tweet_input = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div/div/div/div')
+        except:
+            username = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input')
+            username.send_keys(f"{user}")
+
+            password = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input')
+            password.send_keys(f"{p}")
+
+            button = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div/div')
+            button.click()
+
+            time.sleep(2)
+
+        tweet_input = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/label/div[1]/div/div/div/div/div/div/div/div/div')
+        tweet_input.send_keys(f"Hey! Why am I getting {self.down}/{self.up} mbps when I hired {d}/{u} mbps!!")
+
+        tweet_btn = self.driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[2]/div/div[2]/div[1]/div/div/div/div[2]/div[3]/div/div/div[2]/div[3]')
+        tweet_btn.click()
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from internetspeedtwitterbot import InternetSpeedTwitterBot
 
-PROMISED_UP = 10
-PROMISED_DOWN = 100
-TWITTER_EMAIL = ""
-TWITTER_PASSWORD = ""
+PROMISED_UP = None # YOUR PROMISED DOWNLOAD SPEED
+PROMISED_DOWN = None # YOUR PROMISED UPLOAD SPEED
+TWITTER_EMAIL = "YOUR EMAIL"
+TWITTER_USERNAME = "YOUR USERNAME"
+TWITTER_PASSWORD = "YOUR PASSWORD"
 
 bot = InternetSpeedTwitterBot()
 
 bot.get_internet_speed()
 
 if bot.down < PROMISED_DOWN and bot.up < PROMISED_UP:
-    bot.tweet_at_provider()
+    bot.tweet_at_provider(TWITTER_EMAIL, TWITTER_PASSWORD, TWITTER_USERNAME, u=PROMISED_UP, d=PROMISED_DOWN)
 
-bot.driver.quit()
+# bot.driver.quit()
